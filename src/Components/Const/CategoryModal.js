85a44157da9d832d4ca3b57b276567b562f86394
Shawlycode <kutinaorlando@gmail.com>
2022-01-06 02:53:18 +0000

    added category modal screen

---
+++ src/Components/Const/CategoryModal.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const CategoryModal = () => {
+  return (
+    <View>
+      <Text>Category Modal</Text>
+    </View>
+  );
+};
+
+export default CategoryModal;
+
+const styles = StyleSheet.create({});
