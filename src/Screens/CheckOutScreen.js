18d81dd4665af1eeb8ec049144469bddc7926646
Shawlycode <kutinaorlando@gmail.com>
2022-01-14 01:31:30 +0000

    added  check out component

--- src/Screens/CheckOutScreen.js
+++ src/Screens/CheckOutScreen.js
@@ -1,14 +1,26 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
 
-const CheckOutScreen = () => {
+const CheckOutScreen = ({ navigation }) => {
   return (
-    <View>
-      <Text>CHECK OUT PAGE</Text>
+    <View style={styles.container}>
+      <View style={{ flexDirection: "row" }}>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <AntDesign name="arrowleft" size={30} color="red" />
+        </TouchableOpacity>
+      </View>
+      <View></View>
     </View>
   );
 };
 
 export default CheckOutScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 50,
+    marginLeft: 20,
+  },
+});
