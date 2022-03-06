c30f5f26603dd1c9269fa3c0ae3a65bdefd7b11c
Shawlycode <kutinaorlando@gmail.com>
2022-03-06 16:37:18 +0000

    added modal to screen

--- src/Screens/Sell.js
+++ src/Screens/Sell.js
@@ -1,5 +1,6 @@
+import React, { useState } from "react";
+
 import { FontAwesome, Ionicons, MaterialIcons } from "@expo/vector-icons";
-import React from "react";
 import {
   SafeAreaView,
   StyleSheet,
@@ -7,13 +8,195 @@ import {
   TouchableOpacity,
   View,
   TextInput,
+  Modal,
+  Alert,
+  Pressable,
+  FlatList,
+  Image,
 } from "react-native";
 
 import ImagePicker from "../Components/Const/ImagePicker";
+import { showMessage, hideMessage } from "react-native-flash-message";
 
-const Sell = ({ navigation }) => {
+export default function Sell({ navigation }) {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const [categoryList, setCategoryList] = useState([
+    {
+      id: "883228hi",
+      type: "Cars",
+      image: require("../../assets/troy.jpg"),
+      about: "this is the best so far in the market",
+      price: "$200",
+      review: "243 reviews",
+      ratings: "4.5 stars",
+      Image: require("../../assets/sport-car.png"),
+    },
+    {
+      id: "883hi",
+      type: "Camera's",
+      image: require("../../assets/troy.jpg"),
+      about: "this is the best so far in the market",
+      price: "$200",
+      review: "243 reviews",
+      ratings: "4.5 stars",
+      Image: require("../../assets/camera.png"),
+    },
+    {
+      id: "8838hi",
+      type: "Clothes",
+      image: require("../../assets/troy.jpg"),
+      about: "this is the best so far in the market",
+      price: "$200",
+      review: "243 reviews",
+      ratings: "4.5 stars",
+      Image: require("../../assets/fashion-design.png"),
+
+    },
+    {
+      id: "883228h",
+      type: "Toys",
+      image: require("../../assets/troy.jpg"),
+      about: "this is the best so far in the market",
+      price: "$200",
+      review: "243 reviews",
+      ratings: "4.5 stars",
+      Image: require("../../assets/toys.png"),
+
+    },
+    {
+      id: "8832hi",
+      type: "Electronics",
+      image: require("../../assets/jacket.jpg"),
+      about: "this is the best so far in the market",
+      price: "$200",
+      review: "243 reviews",
+      ratings: "4.5 stars",
+      Image: require("../../assets/electronics.png"),
+    },
+    {
+      id: "8832hi",
+      type: "Phones",
+      image: require("../../assets/jacket.jpg"),
+      about: "this is the best so far in the market",
+      price: "$200",
+      review: "243 reviews",
+      ratings: "4.5 stars",
+      Image: require("../../assets/iphone.png"),
+    },
+    {
+      id: "8832hi",
+      type: "Foot Wears",
+      image: require("../../assets/jacket.jpg"),
+      about: "this is the best so far in the market",
+      price: "$200",
+      review: "243 reviews",
+      ratings: "4.5 stars",
+      Image: require("../../assets/shoes.png"),
+    },
+    {
+      id: "8832hi",
+      type: "Jewelery",
+      image: require("../../assets/jacket.jpg"),
+      about: "this is the best so far in the market",
+      price: "$200",
+      review: "243 reviews",
+      ratings: "4.5 stars",
+      Image: require("../../assets/treasure.png"),
+    },
+    {
+      id: "8832hi",
+      type: "Accessories",
+      image: require("../../assets/jacket.jpg"),
+      about: "this is the best so far in the market",
+      price: "$200",
+      review: "243 reviews",
+      ratings: "4.5 stars",
+      Image: require("../../assets/wireless-headphones.png"),
+    },
+    {
+      id: "86665832hi",
+      type: "Foot Wears",
+      image: require("../../assets/jacket.jpg"),
+      about: "this is the best so far in the market",
+      price: "$200",
+      review: "243 reviews",
+      ratings: "4.5 stars",
+      Image: require("../../assets/shoes.png"),
+    },
+    {
+      id: "883hi",
+      type: "Jewelery",
+      image: require("../../assets/jacket.jpg"),
+      about: "this is the best so far in the market",
+      price: "$200",
+      review: "243 reviews",
+      ratings: "4.5 stars",
+      Image: require("../../assets/treasure.png"),
+    },
+    {
+      id: "8832hidgfdg",
+      type: "Accessories",
+      image: require("../../assets/jacket.jpg"),
+      about: "this is the best so far in the market",
+      price: "$200",
+      review: "243 reviews",
+      ratings: "4.5 stars",
+      Image: require("../../assets/wireless-headphones.png"),
+    },
+  ]);
   return (
     <SafeAreaView style={{ backgroundColor: "grey", flex: 1 }}>
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}
+        onBackdropPress={() => {
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <View
+              style={{
+                width: "100%",
+                justifyContent: "space-evenly",
+                marginTop: 20,
+              }}
+            >
+              <FlatList
+                showsHorizontalScrollIndicator={false}
+                horizontal={false}
+                numColumns={3}
+                data={categoryList}
+                renderItem={({ item }) => (
+                  <TouchableOpacity style={styles.modalCon}>
+                    <Image
+                      source={item.Image}
+                      style={{ width: 80, height: 80, resizeMode: "contain" }}
+                    />
+                    <Text style={styles.categoryText}>{item.type}</Text>
+                  </TouchableOpacity>
+                )}
+              />
+            </View>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}
+            >
+              <Ionicons
+                name="close-circle-sharp"
+                size={30}
+                color="red"
+                style={{ position: "absolute", left: 175, top: -540 }}
+              />
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
       <View>
         <Text
           style={{
@@ -44,19 +227,37 @@ const Sell = ({ navigation }) => {
             keyboardType={"numeric"}
           />
         </View>
-        <TouchableOpacity
-          style={styles.formCon}
-          onPress={() => navigation.navigate("CategoryModal")}
-        >
-          <MaterialIcons name="vertical-split" size={30} color="black" />
-          <Text style={styles.placeholder}> Select Category</Text>
-        </TouchableOpacity>
+        <View>
+          <TouchableOpacity
+            style={styles.formCon}
+            onPress={() => setModalVisible(true)}
+          >
+            <Ionicons name="options" size={30} color="black" />
+          </TouchableOpacity>
+          <TextInput placeholder="Category :" style={styles.placeholder} />
+        </View>
+
         <View style={styles.formCon}>
           <MaterialIcons name="description" size={30} color="black" />
           <TextInput placeholder="Description :" style={styles.placeholder} />
         </View>
         <View style={{ flex: 2 }}>
-          <TouchableOpacity style={styles.postBtn}>
+          <TouchableOpacity
+            style={styles.postBtn}
+            onPress={() => {
+              showMessage({
+                message: "Listing Successful",
+                description: "Your add has been posted successfully",
+                type: "success",
+                backgroundColor: "tomato",
+                color: "#fff",
+                hideStatusBar: true,
+                animated: true,
+                animationDuration: 1500,
+                duration: 3000,
+              });
+            }}
+          >
             <Text style={styles.postText}>Post</Text>
           </TouchableOpacity>
           <TouchableOpacity
@@ -71,9 +272,7 @@ const Sell = ({ navigation }) => {
       </View>
     </SafeAreaView>
   );
-};
-
-export default Sell;
+}
 
 const styles = StyleSheet.create({
   imgCon: {
@@ -85,10 +284,7 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     marginLeft: 20,
     marginTop: 28,
-    alignItems: "center"
-
-
-
+    alignItems: "center",
   },
   placeholder: { flex: 1, fontSize: 24, marginLeft: 10 },
   formCon: {
@@ -100,6 +296,45 @@ const styles = StyleSheet.create({
     padding: 10,
     marginBottom: 20,
     marginBottom: 20,
+  },
+  modalView: {
+    marginTop: 25,
+    position: "absolute",
+    top: 10,
+    backgroundColor: "#EDE0FC",
+    borderRadius: 15,
+    padding: 10,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    width: "100%",
+    height: "65%",
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  modalCon: {
+    backgroundColor: "green",
+    borderWidth: 0.6,
+    borderColor: "tomato",
+    margin: 4,
+    width: 120,
+    height: 120,
+    justifyContent: "center",
+    alignItems: "center",
+    shadowOpacity: 0.5,
+    shadowRadius: 6,
+    elevation: 3,
+    borderRadius: 10
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
   },
   postBtn: {
     marginTop: 30,
@@ -123,5 +358,18 @@ const styles = StyleSheet.create({
     width: "90%",
     height: 60,
     borderRadius: 10,
+  },
+  categoryCon: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: 85,
+    height: 40,
+    backgroundColor: "tomato",
+    borderRadius: 5,
+  },
+  categoryText: {
+    fontSize: 18,
+    color: "yellow",
+    fontWeight: "bold",
   },
 });
