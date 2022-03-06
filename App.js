38c18e4971f8ba52fbd3ed037328bb3f2eaf7357
Shawlycode <kutinaorlando@gmail.com>
2022-03-06 16:37:59 +0000

    rendered flashMessage component

--- App.js
+++ App.js
@@ -1,9 +1,28 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, StyleSheet } from "react-native";
 import Route from "./src/Screens/Navigators/Route";
+import FlashMessage from "react-native-flash-message";
 
 const App = () => {
-  return <Route />;
+  return (
+    <View style={{ flex: 1 }}>
+      <Route />
+      <FlashMessage position="relative" style={styles.flash} />
+    </View>
+  );
 };
 
+const styles = StyleSheet.create({
+  flash: {
+    flex: 1,
+    position: "absolute",
+    top: 300,
+    left: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 5,
+    opacity: 1,
+    fontWeight: "bold",
+  },
+});
 export default App;
